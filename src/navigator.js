adc2756c8823bcfcfb8ded35fc3328e8e8a812d5
F. Selçuk Can <facan@dfds.com>
2020-12-23 12:15:17 +0300

    returned spec

--- src/navigator.js
+++ src/navigator.js
@@ -1,26 +1,37 @@
-export function useNavigator() {
-    const {
-        appCodeName,
-        appName,
-        appVersion,
-        platform,
-        product,
-        productSub,
-        userAgent,
-        vendor,
-        vendorSub,
-        taintEnabled,
-        oscpu,
-        language,
-        languages,
-        cookieEnabled,
-        deviceMemory,
-        onLine,
-        maxTouchPoints,
-        doNotTrack,
-        buildID,
-        hardwareConcurrency,
-    } = navigator;
+import { useState, useEffect } from "react";
+
+export function useNavigator(dependencies = []) {
+    const [spec, setSpec] = useState();
+
+    useEffect(() => {
+        const spec = {
+            appCodeName,
+            appName,
+            appVersion,
+            platform,
+            product,
+            productSub,
+            userAgent,
+            vendor,
+            vendorSub,
+            taintEnabled,
+            oscpu,
+            language,
+            languages,
+            cookieEnabled,
+            deviceMemory,
+            onLine,
+            maxTouchPoints,
+            doNotTrack,
+            buildID,
+            hardwareConcurrency,
+        } = navigator;
+
+        setSpec(spec);
+    }, dependencies);
+
+
+    return spec;
 }
 
 // https://html.spec.whatwg.org/multipage/system-state.html#
