0ff48f919fe618192df6f2b0a5fb988a6acc8e7a
F. Selçuk Can <facan@dfds.com>
2020-12-23 12:01:34 +0300

    added location hook

---
+++ src/location.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+
+export function useLocation(dependencies = [].concat(navigator.location)) {
+    const [spec, setSpec] = useState();
+
+    useEffect(() => {
+        setSpec(navigator.location);
+    }, dependencies);
+}
