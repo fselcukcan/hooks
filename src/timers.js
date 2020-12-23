7f39c4664a06ee20a7aa11e67995e96435dafd18
F. Selçuk Can <facan@dfds.com>
2020-12-23 11:56:35 +0300

    added dependencies as callback and timeout

--- src/timers.js
+++ src/timers.js
@@ -1,6 +1,6 @@
-export function useTimeout(callback, timeout) {
+export function useTimeout(callback, timeout, dependencies = []) {
     useEffect(() => {
         const id = window.setTimeout(callback, timeout);
         return () => window.clearTimeout(id);
-    }); // ? callback, timeout
+    }, [callback, timeout, ...dependencies]); // ? callback, timeout
 }
